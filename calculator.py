f7e9844eb39a77ba7d6739ac9892c090e9382393
Marat <brutto061@gmail.com>
2023-02-11 15:31:13 +0700

    поправили код

--- calculator.py
+++ calculator.py
@@ -1,31 +1,32 @@
-example = input('Введите выражение: ')
-example = example.replace(' ', '').replace('+', ' + ').\
-    replace('-', ' - ').replace('*', ' * ').replace('/', ' / ')
+# example = input('Введите выражение, испоьзуя знаки +, -, *, /: ')
 
-if example.startswith(' - '):
-    example = '-' + example[3:]
+# example = example.replace(' ', '').replace('+', ' + ').\
+#     replace('-', ' - ').replace('*', ' * ').replace('/', ' / ')
+# if example.startswith(' - '):
+#     example = '-' + example[3:]
+# example = example.split()
 
-example = example.split()
-origin_example = example.copy()
-operation = {'+': lambda x, y: x + y,
-             '-': lambda x, y: x - y,
-             '*': lambda x, y: x * y,
-             '/': lambda x, y: x / y,}
+# origin_example = example.copy()
 
-def calculate(oper_1, oper_2):
-    i = 0
-    while oper_1 in example or oper_2 in example:
-        if example[i] in [oper_1, oper_2]:
-            example[i-1] = operation.get(example[i])(int(example[i -1]), int(example[i + 1]))
-            example.pop(i)
-            example.pop(i)
-        else:
-            i += 1
+# operation = {'+': lambda x, y: x + y,
+#              '-': lambda x, y: x - y,
+#              '*': lambda x, y: x * y,
+#              '/': lambda x, y: x / y,}
 
-while len(example) > 1:
-    calculate('/', '*')
-    calculate('+', '-')
+# def calculate(oper_1, oper_2):
+#     i = 0
+#     while oper_1 in example or oper_2 in example:
+#         if example[i] in [oper_1, oper_2]:
+#             example[i-1] = operation.get(example[i])(int(example[i -1]), int(example[i + 1]))
+#             example.pop(i)
+#             example.pop(i)
+#         else:
+#             i += 1
 
-print(f'{" ".join(origin_example)} = {example[0]}')
+# while len(example) > 1:
+#     calculate('/', '*')
+#     calculate('+', '-')
+
+# print(f'{" ".join(origin_example)} = {example[0]}')
 
 
--- controller.py
+++ controller.py
@@ -1,17 +1,17 @@
 import ui
-import operator
+import oper
 
 
 def start():
     #example = view.get_examp()
-    correct_example = operator.concat_examp(ui.get_examp())
+    correct_example = oper.concat_examp(ui.get_examp())
 
-    origin_example = operator.copy_examp(correct_example)
+    origin_example = oper.copy_examp(correct_example)
 
     
     while len(correct_example) > 1:
-        result = operator.calculate('/', '*', correct_example)
-        result = operator.calculate('+', '-', correct_example)
+        result = oper.calculate('/', '*', correct_example)
+        result = oper.calculate('+', '-', correct_example)
         
 
     ui.print_result(origin_example, result)
